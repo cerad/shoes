144058c307738ce82e81e0e2d857c2994d93b12c
ahundiak <ahundiak@gmail.com>
2020-06-21 11:51:35 -0500

    Basic Shoe Listing

---
+++ src/Controller/ShoeController.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\ShoeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShoeController extends AbstractController
+{
+    private ShoeRepository $shoeRepository;
+
+    public function __construct(ShoeRepository $shoeRepository)
+    {
+        $this->shoeRepository = $shoeRepository;
+    }
+
+    /**
+     * @Route("/shoe/list", name="shoe_list")
+     */
+    public function list()
+    {
+        $shoes = $this->shoeRepository->findAll();
+
+        return $this->render('shoe/shoe_list.html.twig', [
+            'shoes' => $shoes,
+        ]);
+    }
+}
