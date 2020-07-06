29873fca44507d95cd25ae4101c85ec932d27445
ahundiak <ahundiak@gmail.com>
2020-07-06 06:53:01 -0500

    Some action nonsense

---
+++ cerad-common/Action/RouterTrait.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Cerad\Common\Action;
+
+
+trait RouterTrait
+{
+
+}
---
+++ src/ShoeStore/Table/MyFormView.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace App\ShoeStore\Table;
+
+class MyFormView
+{
+    protected \Symfony\Component\Form\FormView $formView;
+
+}
